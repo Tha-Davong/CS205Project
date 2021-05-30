c7e9d4c2f3c008c22a0bfad0dbf493010f754e6c
Tha-Davong <77258709+Tha-Davong@users.noreply.github.com>
2021-05-30 22:30:49 +0700

    Add files via upload

--- Project/matrix.hpp
+++ Project/matrix.hpp
@@ -21,6 +21,10 @@ public:
         mat_ptr = new T[row * col];
     }
 
+    Matrix() {
+        
+    }
+
     
     ~Matrix() {
 
@@ -96,6 +100,193 @@ public:
         return SliceRes;
 
     }
+    T Max(){
+        T maxVal = mat_ptr[0];
+
+        for (int i = 0; i < size; ++i) {
+            if (maxVal < mat_ptr[i]) {
+                maxVal = mat_ptr[i];
+            }
+        }
+        return maxVal;
+    }
+
+    Matrix<T> Max(int axis) {
+        
+        if (axis == 0) {
+            Matrix<T> maxMatrix(1, col);
+            
+            int maxVal = 0;
+            for (int i = 0; i < col; ++i) {
+                maxVal = get(0, i);
+                for (int j = 0; j < row; ++j) {
+                    if (maxVal < get(j, i)) {
+                        maxVal = get(j, i);
+                    }
+                }
+                maxMatrix.get(0, i) = maxVal;
+            }
+            return maxMatrix;
+        }
+        else if (axis == 1) {
+            Matrix<T> maxMatrix(row, 1);
+            int maxVal = 0;
+            for (int i = 0; i < row; ++i) {
+                maxVal = get(i, 0);
+                for (int j = 0; j < col; ++j) {
+                    if (maxVal < get(i, j)) {
+                        maxVal = get(i, j);
+                    }
+                }
+                maxMatrix.get(i, 0) = maxVal;
+            }
+            return maxMatrix;
+        }
+
+        
+        
+    }
+
+    T Min() {
+        T maxVal = mat_ptr[0];
+
+        for (int i = 0; i < size; ++i) {
+            if (maxVal > mat_ptr[i]) {
+                maxVal = mat_ptr[i];
+            }
+        }
+        return maxVal;
+    }
+
+    Matrix<T> Min(int axis) {
+
+        if (axis == 0) {
+            Matrix<T> minMatrix(1, col);
+
+            int maxVal = 0;
+            for (int i = 0; i < col; ++i) {
+                maxVal = get(0, i);
+                for (int j = 0; j < row; ++j) {
+                    if (maxVal > get(j, i)) {
+                        maxVal = get(j, i);
+                    }
+                }
+                minMatrix.get(0, i) = maxVal;
+            }
+            return minMatrix;
+        }
+        else if (axis == 1) {
+            Matrix<T> minMatrix(row, 1);
+            int maxVal = 0;
+            for (int i = 0; i < row; ++i) {
+                maxVal = get(i, 0);
+                for (int j = 0; j < col; ++j) {
+                    if (maxVal > get(i, j)) {
+                        maxVal = get(i, j);
+                    }
+                }
+                minMatrix.get(i, 0) = maxVal;
+            }
+            return minMatrix;
+        }
+
+
+
+    }
+
+    T Avg() {
+        T Avg = 0;
+
+        for (int i = 0; i < size; ++i) {
+            Avg += mat_ptr[i];
+        }
+        Avg /= size;
+        return Avg;
+    }
+
+    Matrix<T> Avg(int axis) {
+
+        if (axis == 0) {
+            Matrix<T> AvgMatrix(1, col);
+
+            int avg = 0;
+            for (int i = 0; i < col; ++i) {
+                avg = 0;
+                for (int j = 0; j < row; ++j) {
+                    
+                   avg += get(j, i);
+                    
+                }
+                avg /= row;
+                AvgMatrix.get(0, i) = avg;
+            }
+            return AvgMatrix;
+        }
+        else if (axis == 1) {
+            Matrix<T> AvgMatrix(row, 1);
+            int avg = 0;
+            for (int i = 0; i < row; ++i) {
+                avg = 0;
+                for (int j = 0; j < col; ++j) {
+                    
+                        avg += get(i, j);
+                   
+                }
+                avg /= col;
+                AvgMatrix.get(i, 0) = avg;
+            }
+            return AvgMatrix;
+        }
+    
+    }
+
+    T Sum() {
+        T Sum = 0;
+
+        for (int i = 0; i < size; ++i) {
+           Sum += mat_ptr[i];
+        }
+       
+        return Sum;
+    }
+
+    Matrix<T> Sum(int axis) {
+
+        if (axis == 0) {
+            Matrix<T> SumMatrix(1, col);
+
+            int sum = 0;
+            for (int i = 0; i < col; ++i) {
+                sum = 0;
+                for (int j = 0; j < row; ++j) {
+
+                    sum += get(j, i);
+
+                }
+                
+                SumMatrix.get(0, i) = sum;
+            }
+            return SumMatrix;
+        }
+        else if (axis == 1) {
+            Matrix<T> SumMatrix(row, 1);
+            int sum = 0;
+            for (int i = 0; i < row; ++i) {
+                sum = 0;
+                for (int j = 0; j < col; ++j) {
+
+                    sum += get(i, j);
+
+                }
+               
+                SumMatrix.get(i, 0) = sum;
+            }
+            return SumMatrix;
+        }
+
+    }
+
+    
 };
 
 
