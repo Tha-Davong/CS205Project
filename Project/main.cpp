80fd9426c43f6e3e78d787b2cab6ad9ca6ebd41d
Tha Davong <11910227@mail.sustech.edu.cn>
2021-06-03 20:57:05 +0700

    test

--- Project/main.cpp
+++ Project/main.cpp
@@ -4,13 +4,15 @@
 using namespace std;
 
 int main() {
+
+    //this is the testing branch
     Matrix<int> m(3, 3), m1(2, 2);
     int a[] = { 1,2,3,4,5,6,7,8,9 };
     int b[] = { 1,2,3,4 };
     m.set(9, a);
     m1.set(4, b);
     Matrix<int> c = m.Convolve(m1);
-    //snow 2
+    
     c.print();
     return 0;
 }
