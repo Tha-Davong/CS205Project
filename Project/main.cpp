f31eb8e5869549a0acfc85fa32b666ba7335d637
Tha Davong <11910227@mail.sustech.edu.cn>
2021-06-06 13:34:53 +0700

    before merging with master

--- Project/main.cpp
+++ Project/main.cpp
@@ -5,19 +5,17 @@ using namespace std;
 
 int main() {
     //this is the testing branch
-    Matrix<int> m1(4, 4), m2(2, 1);
+    Matrix<int> m1(4, 4), m2(2, 3);
     int a[] = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16 };
-    int b[] = { 1,2};
+    int b[] = { 1,2,3,4,5,6};
     m1.set(16, a);
-    m2.set(2, b);
+    m2.set(6, b);
     Matrix<int> c = m1.Convolve(m2);
     
     c.print();
     cout << endl;
-    c.reshape(4,3);
+    c.reshape(2,3);
     c.print();
-   cout << endl;
-  Matrix<int> slice = c.Slice(0,1,1,1,2,1);
-  slice.print();
+    c.Avg(1).print();
     return 0;
 }
